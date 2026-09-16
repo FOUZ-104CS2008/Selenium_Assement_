c0886ee8023ef5eda084a440bd3391942817e83d
B FOUZIYA <110445840+FOUZ-104CS2008@users.noreply.github.com>
2026-09-16 23:20:40 +0530

    Create sauceDemoExecutor.java

---
+++ Day9_Assessment/sauceDemoExecutor.java
@@ -0,0 +1,30 @@
+package KeywordDrive;
+
+public class sauceDemoExecutor {
+	seucoDemoImplemation s=new seucoDemoImplemation();
+	public void executor(String keyword) {
+		if (keyword.equals("LAUNCH_BROWSER")) {
+            s.launchBrowser();
+        }
+
+        else if (keyword.equals("OPEN_URL")) {
+            s.openurl();
+        }
+        else if (keyword.equals("USER_TF")) {
+            s.usertf();
+        }
+        else if (keyword.equals("PASS_TF")) {
+            s.passtf();
+        }
+        else if (keyword.equals("LOGIN_BUTTON")) {
+            s.loginButton();
+        }
+        else if (keyword.equals("CLOSE_BROWSER")) {
+            s.closeBrowser();
+		
+		
+		
+        }
+	}
+
+}
